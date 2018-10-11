1da795830ebc53cb0b5f78acf2a0483d9067c28b
ynn <ynn@mel.yt>
2018-10-11 15:43:12 +0200

    wip

--- src/uml/exo1/Test.java
+++ src/uml/exo1/Test.java
@@ -13,14 +13,14 @@ public class Test {
     Personne paul = new Personne("Paul");
     paul.setAge(27);
 
-    Personne georgette = paul.faireEnfantAvec(marie, "Georgette");
+    Personne tom = paul.faireEnfantAvec(marie, "Georgette");
 
     System.out.println(paul.toString());
     System.out.println(marie.toString());
-    System.out.println(georgette);
+    System.out.println(tom);
 
     System.out.println("Les parents de georgettes sont :");
-    List<Personne> parents = georgette.getParents();
+    List<Personne> parents = tom.getParents();
     for (Personne p : parents) {
       System.out.println(p.getNom());
     }
---
+++ src/uml/exo2/Etat.java
@@ -0,0 +1,61 @@
+package uml.exo2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Etat {
+	private String capitale;
+	private String nom;
+	private List<Etat> voisins;
+
+	public Etat(String capitale, String nom, List<Etat> voisins) {
+		super();
+		this.capitale = capitale;
+		this.nom = nom;
+		this.voisins = voisins;
+	}
+
+	public Etat(String capitale, String nom) {
+		this(capitale, nom, new ArrayList<Etat>());
+	}
+
+	public String getCapitale() {
+		return capitale;
+	}
+
+	public void setCapitale(String capitale) {
+
+		this.capitale = capitale;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		if ((nom != null) && (nom.isEmpty())) {
+			this.nom = nom;
+		} else {
+			throw new IllegalArgumentException("le nom n'est pas correct");
+		}
+	}
+
+	public List<Etat> getVoisins() {
+		return voisins;
+	}
+
+	public void setVoisins(List<Etat> voisins) {
+		this.voisins = voisins;
+	}
+
+	@Override
+	public String toString() {
+		return "Etat [capitale=" + capitale + ", nom=" + nom + "]";
+	}
+
+	public void ajouterVoisin(Etat voisin) {
+		voisin.voisins.add(this);
+		this.voisins.add(voisin);
+
+	}
+}
