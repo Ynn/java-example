d3a36f49e4d5fa41991bc83b222a255bf0e58637
ynn <ynn@mel.yt>
2018-10-24 15:49:51 +0200

    generate class diagram

---
+++ src/equalshashcode/exo/Personne.java
@@ -0,0 +1,23 @@
+package equalshashcode.exo;
+
+public class Personne {
+	int age;
+	String nom;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+}
